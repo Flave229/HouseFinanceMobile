54266306e217d9bb16bb49d76b387920efe13dd2
Malcom-Nubbins <malcom.nubbins@gmail.com>
2017-08-16 15:44:15 +0100

    Long bill names should now wrap properly.

--- HouseFinance/src/main/java/hoppingvikings/housefinancemobile/UserInterface/Items/BillObjectDetailedPayments.java
+++ HouseFinance/src/main/java/hoppingvikings/housefinancemobile/UserInterface/Items/BillObjectDetailedPayments.java
@@ -3,7 +3,9 @@ package hoppingvikings.housefinancemobile.UserInterface.Items;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by iView on 15/08/2017.
@@ -15,6 +17,7 @@ public class BillObjectDetailedPayments {
     public String personName = "";
     public String Date = "";
     public double AmountPaid = 0.0;
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.UK);
 
     public BillObjectDetailedPayments(JSONObject paymentObject, String billID)
     {
